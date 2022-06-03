7b500e7007a4ceb8d198acc9dd97dad900f3fa48
FlutLab.io <>
2022-06-03 09:00:50 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return MaterialApp(theme: ThemeData(primaryColor: Colors.purple[400]), home: Scaffold(appBar: AppBar(title: Text('WordPair Generator')), body: Center(child: Text(wordPair.asPascalCase))));
+  }
+}
