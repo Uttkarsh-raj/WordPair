81827683c2fc08382c5c5addb43ab670416dc4c0
FlutLab.io <>
2022-06-03 09:20:01 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,40 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final wordPair = WordPair.random();
-    return MaterialApp(theme: ThemeData(primaryColor: Colors.purple[400]), home: Scaffold(appBar: AppBar(title: Text('WordPair Generator')), body: Center(child: Text(wordPair.asPascalCase))));
+    return MaterialApp(theme: ThemeData(primaryColor: Colors.purple[900]), home: RandomWords());
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  Widget _buildList() {
+    return ListView(
+      padding: const EdgeInsets.all(8),
+      children: <Widget>[
+        Container(
+          height: 50,
+          color: Colors.amber[600],
+          child: const Center(child: Text('Entry A')),
+        ),
+        Container(
+          height: 50,
+          color: Colors.amber[500],
+          child: const Center(child: Text('Entry B')),
+        ),
+        Container(
+          height: 50,
+          color: Colors.amber[100],
+          child: const Center(child: Text('Entry C')),
+        ),
+      ],
+    );
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text('WordPair Generator')), body: _buildList());
   }
 }
